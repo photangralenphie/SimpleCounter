a1d33e6ae20e35208979884d11196fffc02cc7e6
Jonas Helmer <64361476+photangralenphie@users.noreply.github.com>
2023-01-04 22:43:04 +0100

    Initial Commit

---
+++ SimpleCounter/SimpleCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCounterApp.swift
+//  SimpleCounter
+//
+//  Created by Jonas Helmer on 04.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
